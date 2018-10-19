fdb143451971123dd3cbe90e2e283520ae95d7ab
John Werenski <jtwerenski@gmail.com>
2018-10-19 19:11:20 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -86,6 +86,7 @@ class Game
       player_1 = Players::Computer.new("X")
       player_2 = Players::Human.new("O")
       Game.new(player_1, player_2).play
+      game_over
       
     elsif input == 2 
       player_1 = Players::Human.new("X")
