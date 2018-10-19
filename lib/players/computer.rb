c60860a32f66b6b906bde2c3bef4e6b9ac98723e
John Werenski <jtwerenski@gmail.com>
2018-10-19 15:38:48 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -181,6 +181,11 @@ module Players
         nil
       end 
     end 
+    
+    def edge_move(board)
+      if board.cells[0] != " " && board.cells[8] != " "
+        "4"
+      elsif board.cells[6] != " " && board.cells[2] !=
         
   end 
 end 
